761abe8b771371a4172c034b039e69d51f6b9433
chiudaniels <chiudaniels@gmail.com>
2017-02-26 23:11:26 -0500

    :)

--- canvas.js
+++ canvas.js
@@ -1,24 +1,72 @@
 var canv= document.getElementById("area");
 var ctx= canv.getContext("2d"); 
-var x=canv.width/2;
-var y=canv.height/2;
+var x=Math.random() * canv.width;
+var y=Math.random() * canv.height;
+var requestID;
+var size=0;
+var grow=true;
 
-var clear= function(){
-    ctx.clearRect(0,0,500,500);
+var clearCanv = function(){
+    ctx.clearRect(0,0,canv.width,canv.height);
 }
 
-var grow = function(){
-    var size= 0;
-    if size = canv.width/2{
-	clear;
-	ctx.arc(canv.width/2,canv.height/2,size,0,2*Math.PI);
-	size++;
-    }
-    else{
-	clear;
-	ctx.arc(canv.width/2,canv.height/2,size,0,2*Math.PI);
-	size--; 
-    }
-
+var growAnimate = function (){
+	window.cancelAnimationFrame( requestID );
+		
+		var growCircle = function(){
+			clearCanv();
+			ctx.arc(canv.width/2,canv.height/2,size,0,2*Math.PI);
+			ctx.fill();
+			size++;
+			if (size<canv.width/2){
+				requestID = window.requestAnimationFrame(growCircle)
+			}
+			else{
+				requestID = window.requestAnimationFrame(shrinkCircle)
+			}
+		}
+		
+		var shrinkCircle = function(){
+			clearCanv();
+			ctx.arc(canv.width/2,canv.height/2,size,0,2*Math.PI);
+			ctx.fill();
+			size--; 
+			if (size>0){
+				requestID = window.requestAnimationFrame(shrinkCircle)
+			}
+			else{
+				requestID = window.requestAnimationFrame(growCircle)
+			}
+		}
+	
+	requestID= window.requestAnimationFrame(growCircle);
 }
 
+var dvdAnimate= function(){
+	window.cancelAnimationFrame(requestID)
+	
+	xVel=1;
+	yVel=1;
+	
+	var moveDVD = function(){
+		window.cancelAnimationFrame(requestID);
+		clearCanv();
+		ctx.fillRect(x,y,50,50);
+		ctx.fill();
+		if (x >= canv.width - 50 || x<=0){
+			xVel *= -1;
+		}
+		if (y >= canv.height - 50|| y<=0){
+			yVel *= -1;
+		}
+		x += xVel;
+		y += yVel;
+		requestID = window.requestAnimationFrame(moveDVD);
+		console.log(x,y)
+	}
+	requestID = window.requestAnimationFrame(moveDVD);
+}
+
+var stop = function(){
+	window.cancelAnimationFrame(requestID);
+}
